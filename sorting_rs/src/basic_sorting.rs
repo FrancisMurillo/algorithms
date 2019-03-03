47b8a221cc29ecbd7559a156ce91c144b0ba26f9
Francis Murillo <francisavmurillo@gmail.com>
2019-03-03 21:57:17 +0800

    Checkpoint

--- sorting_rs/src/basic_sorting.rs
+++ sorting_rs/src/basic_sorting.rs
@@ -24,9 +24,31 @@ pub fn selection_sort<T: Ord>(mut xs: Vec<T>) -> Vec<T> {
     }
 }
 
+pub fn insertion_sort<T: Ord>(mut xs: Vec<T>) -> Vec<T> {
+    let sorting = Ordering::Greater;
+
+    if xs.is_empty() || xs.len() == 1 {
+        xs
+    } else {
+        for i in 0..(xs.len() - 2) {
+            println!("MEOW");
+            for j in (i + 1)..1 {
+                println!("{:?}", xs[j].cmp(&xs[j - 1]));
+                if xs[j].cmp(&xs[j - 1]) == sorting {
+                    xs.swap(j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        xs
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::selection_sort;
+    use super::{insertion_sort, selection_sort};
 
     #[quickcheck]
     fn selection_sort_should_work(xs: Vec<isize>) -> bool {
@@ -35,4 +57,13 @@ mod tests {
 
         selection_sort(xs) == ys
     }
+
+    #[quickcheck]
+    fn insertion_sort_should_work(xs: Vec<isize>) -> bool {
+        let mut ys = xs.clone();
+        ys.sort();
+
+        insertion_sort(xs) == ys
+    }
+
 }
